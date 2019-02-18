882b4e9e8e0a1248255b89aabc6f1552a7ea6b8a
wgbbiao <wgb237@163.com>
2019-02-18 14:45:51 +0800

    增加[商品标准类目接口],[查询商品标签列表]

--- pdd.go
+++ pdd.go
@@ -1,5 +1,9 @@
 package pdd
 
+import (
+	"encoding/json"
+)
+
 type Pdd struct {
 	Context *Context
 }
@@ -10,7 +14,7 @@ type Config struct {
 }
 
 func NewPdd(c *Config) *Pdd {
-	return &Pdd{ Context: NewContext(c.ClientId, c.ClientSecret) }
+	return &Pdd{Context: NewContext(c.ClientId, c.ClientSecret)}
 }
 
 func (p *Pdd) GetDDK() *DDK {
@@ -19,4 +23,64 @@ func (p *Pdd) GetDDK() *DDK {
 
 func (p *Pdd) GetGoodsAPI() *GoodsAPI {
 	return NewGoodsAPIWithContext(p.Context)
+}
+
+//方法列表
+const (
+	PDDpddGoodsOptGet = "pdd.goods.opt.get"  //查询商品标签列表
+	PDDGoodsCatsGet   = "pdd.goods.cats.get" //商品标准类目接口
+)
+
+//GoodsOpt 商品类别
+type GoodsOpt struct {
+	Level       int    `json:"level"`         //层级，1-一级，2-二级，3-三级，4-四级
+	ParentOptID int    `json:"parent_opt_id"` //id所属父ID，其中，parent_id=0时为顶级节点
+	OptName     string `json:"opt_name"`      //商品标签名
+	OptID       int    `json:"opt_id"`        //商品标签ID
+}
+
+//GoodsOptList 商品类别列表
+type GoodsOptList struct {
+	GoodsOptList []*GoodsOpt `json:"goods_opt_list"`
+}
+
+//PddGoodsOptGet 查询商品标签列表
+func (d *DDK) PddGoodsOptGet(parentOptID int) (res *GoodsOptList, err error) {
+	params := NewParamsWithType(PDDpddGoodsOptGet)
+	params.Set("parent_opt_id", parentOptID)
+	r, err := Call(d.Context, params)
+	if err != nil {
+		return
+	}
+	bytes, err := GetResponseBytes(r, "goods_opt_get_response")
+	res = new(GoodsOptList)
+	err = json.Unmarshal(bytes, res)
+	return
+}
+
+//GoodsCat  商品标准类目
+type GoodsCat struct {
+	Level       int    `json:"level"`         //层级，1-一级，2-二级，3-三级，4-四级
+	ParentCatID int    `json:"parent_cat_id"` //id所属父ID，其中，parent_id=0时为顶级节点
+	OptName     string `json:"cat_name"`      //商品标准类目名
+	CatID       int    `json:"cat_id"`        //商品标准类目ID
+}
+
+//GoodsCatsList 商品标准类目列表
+type GoodsCatsList struct {
+	CatsList []*GoodsCat `json:"goods_cats_list"`
+}
+
+//PddGoodsCatGet 查询商品标准类目列表
+func (d *DDK) PddGoodsCatGet(ParentCatID int) (res *GoodsCatsList, err error) {
+	params := NewParamsWithType(PDDGoodsCatsGet)
+	params.Set("parent_cat_id", ParentCatID)
+	r, err := Call(d.Context, params)
+	if err != nil {
+		return
+	}
+	bytes, err := GetResponseBytes(r, "goods_cats_get_response")
+	res = new(GoodsCatsList)
+	err = json.Unmarshal(bytes, res)
+	return
 }
